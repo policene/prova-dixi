ba2411490e0e9670f48b79cf70c68c13a4a1b3d0
Giovanni <giovannicpoli@gmail.com>
2026-01-09 20:08:38 -0300

    initial commit

---
+++ prova-dixi-api/src/main/java/com/policene/prova_dixi_api/ProvaDixiApiApplication.java
@@ -0,0 +1,13 @@
+package com.policene.prova_dixi_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaDixiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaDixiApiApplication.class, args);
+	}
+
+}
---
+++ prova-dixi-api/src/test/java/com/policene/prova_dixi_api/ProvaDixiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policene.prova_dixi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaDixiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
